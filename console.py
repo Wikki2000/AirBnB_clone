20a89461ca20c51bdae3452bfb1690eb1f29731b
wikki2000 <wisdomokposin@gmail.com>
2024-03-24 08:12:11 -0700

    Resolve error

--- console.py
+++ console.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python3
-""""<HBNBCommand> module: Models the entry point of the command interpreter"""
+"""console.py - Entry point of the HBNB command interpreter"""
 import cmd
 
 
 class HBNBCommand(cmd.Cmd):
-    """This is a subclass for command interpreter"""
-    prompt = '(hbnb) '
+    """This class defines the command interpreter"""
+
+    intro = "Welcome to the HBNB command interpreter. Type 'help' to list commands."
+    prompt = "(hbnb) "
 
     def do_quit(self, args):
-        """This method use to quit program"""
+        """Quit command to exit the program"""
         return True
 
     def do_EOF(self, args):
-        """This method use to quit program"""
+        """Exit the program when EOF is reached (Ctrl+D)"""
         return True
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
+
