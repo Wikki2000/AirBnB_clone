c9abb7ff4b42b8ad4048e2cd5310fc93121c345c
wikki2000 <wisdomOkposin@gmail.com>
2024-03-30 06:00:58 -0700

    Add files'

---
+++ web_static/styles/3-footer.cs
@@ -0,0 +1,11 @@
+footer{
+	background-color: white;
+	height: 60px;
+	width: 100%;
+	display: flex;
+	position: absolute;
+	border-top: 1px solid #CCCCCC;
+	bottom: 0;
+	justify-content: center;
+	align-items: center;
+}
