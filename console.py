e472ece39b380926522ea13ee4d4964d0b462797
wikki2000 <wisdomokposin@gmail.com>
2024-04-17 15:53:35 -0700

    Update file console.py

--- console.py
+++ console.py
@@ -32,6 +32,16 @@ class HBNBCommand(cmd.Cmd):
         """ print instance id is missing message """
         print("** instance id missing **")
 
+    @staticmethod
+    def class_name(args):
+        """Retrieve class name"""
+        args_list = args.split()
+        class_name = args_list[0]  # Get the class name from args_list
+        obj_class = HBNBCommand.__obj_dict[class_name]  # Get the class from obj_dict using the class name
+        return obj_class
+
+
+
     def do_quit(self, args):
         """Quit command to exit the program"""
         return True
@@ -48,7 +58,7 @@ class HBNBCommand(cmd.Cmd):
         elif arg not in HBNBCommand.__obj_dict:
             self.wrong_class()
         else:
-            obj = HBNBCommand.__obj_dict[arg]()
+            obj = HBNBCommand.__obj_dict[arg]() # Create an instance of a class
             obj.save()
             print(obj.id)
 
@@ -63,9 +73,8 @@ class HBNBCommand(cmd.Cmd):
         elif args_list[0] in HBNBCommand.__obj_dict and len(args_list) != 2:
             self.missing_id()
         else:
-            class_name = args_list[0]  # Get the class name from args_list
             instance_id = args_list[1]  # Get the instance ID from args_list
-            obj_class = HBNBCommand.__obj_dict[class_name]  # Get the class from obj_dict using the class name
+            obj_class = self.class_name(args)
 
             for key, obj in obj_storage.items():
                 if isinstance(obj, obj_class) and obj.id == instance_id:
@@ -75,9 +84,7 @@ class HBNBCommand(cmd.Cmd):
                     self.invalid_instance()
 
     def do_destroy(self, args):
-        """Deletes an instance based on 
-        the class name and id (save the change into the JSON file)
-        """
+        """Deletes an instance based on the class name and id (save the change into the JSON file)"""
         obj_storage = storage.all()
         args_list = args.split()
         if len(args_list) == 0:
@@ -87,12 +94,8 @@ class HBNBCommand(cmd.Cmd):
         elif args_list[0] in HBNBCommand.__obj_dict and len(args_list) != 2:
             self.missing_id()
         else:
-            class_name = args_list[0]
+            obj_class = self.class_name(args)
             instance_id = args_list[1]
-            obj_class = HBNBCommand.__obj_dict[class_name]
-
-            # To avoid error flag when resizing an iterable 
-            # Iterate through it copy
             for key, obj in obj_storage.copy().items():
                 if isinstance(obj, obj_class) and obj.id == instance_id:
                     del obj_storage[key]
@@ -100,5 +103,54 @@ class HBNBCommand(cmd.Cmd):
                     break
                 else:
                     self.invalid_instance()
+
+    def do_all(self, args):
+        obj_storage = storage.all()
+        """Print a list of all string rep. of all instances based or not on class name"""
+        obj_list = []
+        args_list = args.split()
+        if len(args_list) == 1:
+            if args_list[0] not in HBNBCommand.__obj_dict:
+                self.wrong_class()
+            else:
+                obj_class = self.class_name(args)
+                for key, obj in obj_storage.items():
+                    if isinstance(obj, obj_class):
+                        obj_list.append(str(obj))
+                print(obj_list)
+        elif len(args_list) == 0:
+            for key, obj in obj_storage.items():
+                obj_list.append(str(obj))
+            print(obj_list)
+
+    def do_update(self, args):
+        """Update an attribute of a class"""
+        obj_storage = storage.all()
+        args_list = args.split()
+        if len(args_list) == 0:
+            self.missing_name()
+        elif len(args_list) == 1 and args_list[0] not in HBNBCommand.__obj_dict:
+            self.wrong_class()
+        elif args_list[0] in HBNBCommand.__obj_dict:
+            if len(args_list) == 1:
+                self.missing_id()
+            elif len(args_list) == 2:
+                print("** attribute name missing **")
+            elif len(args_list) == 3:
+                print("** value missing **")
+            elif len(args_list) == 4:
+
+                # Check for invalid object id
+                obj_class = self.class_name(args)
+                instance_id = args_list[1]
+                for key, obj in obj_storage.items():
+                    if isinstance(obj, obj_class) and obj.id == instance_id:
+                        setattr(obj, args_list[3], args_list[3])
+                        break
+                else:
+                    self.invalid_instance()
+            else:
+                print("** number of arguments exceeded **")
+
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
